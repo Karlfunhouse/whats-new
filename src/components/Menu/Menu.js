8c96f313ff6f79eb3d521d1fd774774fcacc6de5
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-05-05 14:24:26 -0600

    :crystal_ball: Building out Menu component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,5 +2,16 @@ import React from 'react';
 import './Menu.css'
 
 // MENU COMPONENT CODE GOES HERE
+const Menu = () => {
+  return(
+    <nav>
+      <button>Local News</button>
+      <button>Technology</button>
+      <button>Entertainment</button>
+      <button>Science</button>
+      <button>Health</button>
+    </nav>
+  )
+}
 
 export default Menu;
