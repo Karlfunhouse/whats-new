a910cdec168ba07e134f9d3b74b368f30c2b8752
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-05-05 14:25:26 -0600

    :crystal_ball: Adding component structure to display articles using props

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -2,8 +2,15 @@ import React from 'react';
 import './NewsArticle.css';
 
 // NEWSARTICLE COMPONENT CODE GOES HERE
-function NewsArticle() {
-
+const NewsArticle = (props) => {
+  return(
+    <section>
+      <img src={props.img} className="article-image"/>
+      <h1 className="heading">{props.headline}</h1>
+      <h3 className="description">{props.description}</h3>
+      <button>{props.url}</button>
+    </section>
+  )
 }
 
 export default NewsArticle;
