ac4a7473f806c731fd1e67168cfd6bad065ff28e
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-05-05 14:27:20 -0600

    :crystal_ball: Adding component structure to display Search Form/header

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
 // SEARCHFORM COMPONENT CODE GOES HERE
+class SearchForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: null,
+      headline: '',
+      description: '',
+      img: '',
+      url: ''
+    }
+  }
+  //
+  // handleChange = (e) => {
+  //   this.setState()
+  // }
+
+}
 
 export default SearchForm;
