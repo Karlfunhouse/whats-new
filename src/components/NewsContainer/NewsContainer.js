9406ef49da95eb79834e04f19c1e2fab9dbffc32
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-05-06 15:44:54 -0600

    :crystal_ball: Refactored code for it to be DRY & added spread operator to populate each article

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -5,49 +5,18 @@ import NewsArticle from '../NewsArticle/NewsArticle'
 // NEWSCONTAINER COMPONENT CODE GOES HERE
 const NewsContainer = (props) => {
   console.log("props", props);
-  const localArticles =
-  props.local.local.map(article => {
+  const articles =
+  props.currentArticles.map(article => {
     return <NewsArticle
       key={article.id}
       {...article}
     />
   })
 
-  const healthArticles =
-  props.health.health.map(article => {
-    return <NewsArticle
-      key={article.id}
-      {...article}
-    />
-  })
-
-  const entertainmentArticles =
-  props.entertainment.entertainment.map(article => {
-    return <NewsArticle
-      key={article.id}
-      {...article}
-    />
-  })
-
-  const scienceArticles =
-  props.science.science.map(article => {
-    return <NewsArticle
-      key={article.id}
-      {...article}
-    />
-  })
-
-  const technologyArticles =
-  props.technology.technology.map(article => {
-    return <NewsArticle
-      key={article.id}
-      {...article}
-    />
-  })
 
     return (
       <section className="news-container">
-          {localArticles}
+          {articles}
       </section>
     )
 }
