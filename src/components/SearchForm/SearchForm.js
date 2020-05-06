2323c3737a77a0be02827b907594390f8b786152
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-05-06 15:43:17 -0600

    :crystal_ball: created handleChange & findStory methods to take input from search bar and filter stories for matching headlines or descriptions

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,13 +5,24 @@ import './SearchForm.css';
 class SearchForm extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      searchValue: ''
+      }
+    }
+
+    handleChange = (e) => {
+      this.setState({searchValue: e.target.value.toLowerCase()})
+    }
+
+    findStory = (props) => {
+      this.props.searchNews(this.state.searchValue.toLowerCase())
     }
 
     render() {
       return (
         <header className="header">
-          <input type="text" id="search" className="search-bar" name="search" placeholder="Search for news article here." />
-          <button className="search-button">Search</button>
+          <input type="text" id="search" className="search-bar" name="search" placeholder="Search for news article here." onChange={this.handleChange}/>
+          <button className="search-button" onClick={this.findStory}>Search</button>
         </header>
       )
     }
