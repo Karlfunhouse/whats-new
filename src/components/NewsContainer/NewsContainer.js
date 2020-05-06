30d4c910cade0812be3f81dec4c44690bb8b01a9
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-05-06 14:43:48 -0600

    :crystal_ball: Adding other article sources into App state & Passing down as props into NewsContainer

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -5,13 +5,43 @@ import NewsArticle from '../NewsArticle/NewsArticle'
 // NEWSCONTAINER COMPONENT CODE GOES HERE
 const NewsContainer = (props) => {
   console.log("props", props);
-  const localArticles = props.local.local.map(article => {
+  const localArticles =
+  props.local.local.map(article => {
     return <NewsArticle
       key={article.id}
-      headline={article.headline}
-      img={article.img}
-      description={article.description}
-      url={article.url}
+      {...article}
+    />
+  })
+
+  const healthArticles =
+  props.health.health.map(article => {
+    return <NewsArticle
+      key={article.id}
+      {...article}
+    />
+  })
+
+  const entertainmentArticles =
+  props.entertainment.entertainment.map(article => {
+    return <NewsArticle
+      key={article.id}
+      {...article}
+    />
+  })
+
+  const scienceArticles =
+  props.science.science.map(article => {
+    return <NewsArticle
+      key={article.id}
+      {...article}
+    />
+  })
+
+  const technologyArticles =
+  props.technology.technology.map(article => {
+    return <NewsArticle
+      key={article.id}
+      {...article}
     />
   })
 
