1bc9d42987934190fa602ad083327f4b6fea85f5
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-05-06 15:44:12 -0600

    :crystal_ball: Added onClick handlers for each button to fire displayArticles method with each category going in as an argument

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './Menu.css'
 
 // MENU COMPONENT CODE GOES HERE
-const Menu = () => {
+const Menu = (props) => {
   return(
     <aside className="nav-menu">
       <h1 className="title">What's<span className="yellow"> News?</span></h1>
-      <button className="nav-button" id="local-news">Local News</button>
-      <button className="nav-button" id="technology">Technology</button>
-      <button className="nav-button" id="entertainment">Entertainment</button>
-      <button className="nav-button" id="science">Science</button>
-      <button className="nav-button" id="health">Health</button>
+      <button className="nav-button" onClick={() => props.displayArticles('local')}>Local News</button>
+      <button className="nav-button" onClick={() => props.displayArticles('technology')}>Technology</button>
+      <button className="nav-button" onClick={() => props.displayArticles('entertainment')}>Entertainment</button>
+      <button className="nav-button" onClick={() => props.displayArticles("science")}>Science</button>
+      <button className="nav-button" onClick={() => props.displayArticles("health")}>Health</button>
     </aside>
   )
 }
